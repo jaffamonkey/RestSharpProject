6137202a530f453023b976e1f6a65698786cfea5
simon <sj102399@gmail.com>
2020-02-20 14:01:02 -0600

    Added initial project

---
+++ RestSharpProject/Tests/BasicRestTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+using RestSharp;
+
+namespace RestSharpProject.Tests
+{
+    [TestFixture]
+    public class BasicRestTest
+    {
+        [Test]
+        public void StatusCode200Test()
+        {
+            //Arrange
+            RestClient client = new RestClient("http://api.github.com");
+            RestRequest request = new RestRequest("/rate_limit", Method.GET);
+
+            //Act
+            IRestResponse response = client.Execute(request);
+
+            //Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
